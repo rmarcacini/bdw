82575f99155e7c78769b9515c90839e13528a95c
rmarcacini <40037976+rmarcacini@users.noreply.github.com>
2020-02-20 17:38:17 -0300

    Update evaluation.py

--- bdw/evaluation.py
+++ bdw/evaluation.py
@@ -3,7 +3,7 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
 
 
-def get_train_test(G, labeled_nodes, test_size=0.7):
+def get_train_test(G, labeled_nodes):
   X_train = []
   y_train = []
   X_test = []
@@ -33,3 +33,21 @@ def linear_svc(X_train, X_test, y_train, y_test):
 
 def metrics(y_test, y_preds):
   return classification_report(y_test, y_preds, output_dict=True)
+
+
+from sklearn.model_selection import train_test_split
+
+
+def sample_labeled_nodes(G,train_size=0.3):
+  T = []
+  t = []
+  for node in G.nodes():
+    T.append([node])
+    t.append([G.nodes[node]['label']])
+
+  T_train, T_test, t_train, t_test = train_test_split(T, t, test_size=1.0-train_size, random_state=42)
+
+  labeled_nodes = []
+  for s in T_train: labeled_nodes.append(s[0])
+
+  return labeled_nodes
